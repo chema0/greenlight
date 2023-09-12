3ac89d76a022550b6e43880754e6971ea690adef
Chema <dev.josemariamm@gmail.com>
2023-09-12 20:43:21 +0200

    Adding API CRUD operations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/chema0/greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
@@ -29,6 +30,7 @@ type config struct {
 type application struct {
 	config config
 	logger *log.Logger
+	models data.Models
 }
 
 func main() {
@@ -51,14 +53,13 @@ func main() {
 	if err != nil {
 		logger.Fatal(err)
 	}
-
 	defer db.Close()
-
 	logger.Printf("database connection pool established")
 
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
